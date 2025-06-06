3bb615cabcdbab1597ade583b754230dcec7e2e4
Olivier Delrée <olivierdelree@protonmail.com>
2025-06-06 15:05:06 +0100

    CLI: Add motion correction using SIMA

--- src/drim2p/models.py
+++ src/drim2p/models.py
@@ -2,10 +2,86 @@
 #
 # SPDX-License-Identifier: MIT
 
+from __future__ import annotations
+
 import datetime
+import enum
 import pathlib
+import tomllib
+from typing import Any, cast
 
 import pydantic
+
+
+class Strategy(enum.Enum):
+    Markov = "HiddenMarkov2D"
+    Plane = "PlaneTranslation2D"
+    Fourier = "DiscreteFourier2D"
+
+    @classmethod
+    def _missing_(cls, value: Any) -> Any:
+        try:
+            value = value.lower()
+        except AttributeError:
+            return super()._missing_(value)
+
+        for variant in cls:
+            if variant.name.lower() == value:
+                return variant
+
+        return super()._missing_(value)
+
+
+class MotionConfig(pydantic.BaseModel):
+    strategy: Strategy = Strategy.Fourier
+    displacement: tuple[int, int] = (50, 50)
+
+    @classmethod
+    def from_file(cls, path: pathlib.Path) -> MotionConfig:
+        with open(path, "rb") as handle:
+            contents = tomllib.load(handle)
+
+        dictionary = contents.get("motion-correction")
+        if dictionary is None:
+            raise ValueError(
+                f"Cannot parse file '{path}' as a valid motion config. It does not "
+                f"have a 'motion-correction' section."
+            )
+
+        return cls.from_dictionary(dictionary)
+
+    @classmethod
+    def from_dictionary(cls, dictionary: dict[str, Any]) -> MotionConfig:
+        strategy = dictionary.get("strategy")
+        if strategy is None:
+            raise ValueError(
+                "Could not find a strategy from the provided config dictionary."
+            )
+        try:
+            strategy = Strategy(strategy)
+        except ValueError:
+            raise ValueError(
+                f"Could not parse '{strategy}' as a valid strategy. "
+                f"Valid options: {', '.join(variant.name for variant in Strategy)}."
+            ) from None
+
+        displacement = dictionary.get("displacement")
+        if displacement is None:
+            raise ValueError(
+                "Could not find a displacement from the provided config dictionary."
+            )
+        if not hasattr(displacement, "__len__") or len(displacement) != 2:
+            raise ValueError(
+                f"Could not parse '{displacement}' as two displacement values."
+            )
+        try:
+            displacement = cast(tuple[int, int], tuple(map(int, displacement)))
+        except ValueError:
+            raise ValueError(
+                f"Could not parse '{displacement}' as a tuple of integers values."
+            ) from None
+
+        return cls(strategy=strategy, displacement=displacement)
 
 
 class NotesEntry(pydantic.BaseModel):
